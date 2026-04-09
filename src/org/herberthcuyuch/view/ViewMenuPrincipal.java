6f624e90069142b3ca099ac02ef999cfc4a7fe8b
Herberth Cuyuch <hcuyuch-2026046@kinal.edu.gt>
2026-04-09 11:59:15 -0600

    001 Se agregaron opciones al menu

--- src/org/herberthcuyuch/view/ViewMenuPrincipal.java
+++ src/org/herberthcuyuch/view/ViewMenuPrincipal.java
@@ -17,6 +17,11 @@ public class ViewMenuPrincipal {
     public void mostrarMenu(){
         while(opcionMenu == 0){
             System.out.println("Bienvenido, seleccione una opcion.");
+            System.out.println("1. Operaciones matematicas. ");
+            System.out.println("2. Operaciones de texto. ");
+            System.out.println("3. Operaciones de logica.");
+            System.out.println("4. Salir del sistema.");
+      
             opcionMenu = Integer.parseInt(leer.nextLine());
             
         }    
